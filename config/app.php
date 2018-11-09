8b2d3972546c4404ebd2bc66c60a6faac2b1d8c2
Rogério Pereira <rogerio@colmeiatecnologia.com.br>
2018-11-08 16:39:46 -0800

    Configured Laravel Collective

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //LaravelCollective
+        Collective\Html\HtmlServiceProvider::class,
     ],
 
     /*
@@ -209,6 +211,9 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //LaravelCollective
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,12 +10,10 @@
 
     <title>Easy Communication & Technology - CRUD</title>
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{--Styles--}}
+    {!! Html::style('css/app.css') !!}
+    {!! Html::style('css/site/style.min.css') !!}
+    @yield('css')
 </head>
 <body>
     <div id="app">
@@ -29,6 +27,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
